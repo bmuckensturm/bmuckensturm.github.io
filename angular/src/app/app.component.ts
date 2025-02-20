9b2d567193ec15a51a7564b6dc3a72cde1988cd7
bmuckensturm <bmuckensturm@gmail.com>
2025-02-19 22:43:11 -0500

    Adding droppable lists

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDrag } from '@angular/cdk/drag-drop';
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem, } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CdkDrag]
+  imports: [CdkDrag, CdkDropList, CdkDrag]
 })
 export class AppComponent implements OnInit {
-  ids = Array.from(Array(12).keys());
+  todo = [1, 1, 1, 1, 1];
+  done = [2, 2, 2, 2];
 
   ngOnInit(): void {
   }
 
-  onClick(): void {
-    alert('yup');
+  drop(event: any) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data, 
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
